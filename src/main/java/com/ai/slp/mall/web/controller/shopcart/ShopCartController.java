0a85cc0f28e3eb70f3a3be0b2134b43edd69c0a9
JackieLiu <liutong6053@163.com>
2016-05-25 09:45:37 +0800

    添加购物车

---
+++ src/main/java/com/ai/slp/mall/web/controller/shopcart/ShopCartController.java
@@ -0,0 +1,15 @@
+package com.ai.slp.mall.web.controller.shopcart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 购物车操作
+ * Created by jackieliu on 16/5/20.
+ */
+@Controller
+@RequestMapping("/shopcart")
+public class ShopCartController {
+
+    //添加商品
+}
