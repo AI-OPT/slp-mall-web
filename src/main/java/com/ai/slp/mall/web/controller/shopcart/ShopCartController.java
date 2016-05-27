28154d53627d1553771aba2980d0a8ff1db24069
onle <onle@live.cn>
2016-05-27 16:32:28 +0800

    购物车页面

--- src/main/java/com/ai/slp/mall/web/controller/shopcart/ShopCartController.java
+++ src/main/java/com/ai/slp/mall/web/controller/shopcart/ShopCartController.java
@@ -1,22 +1,26 @@
 package com.ai.slp.mall.web.controller.shopcart;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.ai.opt.base.exception.BusinessException;
-import com.ai.opt.base.exception.SystemException;
-import com.ai.opt.base.vo.PageInfo;
 import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
 import com.ai.opt.sdk.web.model.ResponseData;
+import com.ai.paas.ipaas.util.JSonUtil;
 import com.ai.slp.mall.web.controller.search.SearchController;
 import com.ai.slp.order.api.shopcart.interfaces.IShopCartSV;
 import com.ai.slp.order.api.shopcart.param.CartProd;
+import com.ai.slp.order.api.shopcart.param.CartProdInfo;
 import com.ai.slp.order.api.shopcart.param.CartProdOptRes;
-import com.ai.slp.product.api.webfront.param.ProductData;
+import com.ai.slp.order.api.shopcart.param.UserInfo;
 
 import net.sf.json.JSONArray;
 
@@ -28,7 +32,9 @@ import net.sf.json.JSONArray;
 @RequestMapping("/shopcart")
 public class ShopCartController {
 	private static final Logger LOG = Logger.getLogger(SearchController.class);
-    //添加商品
+    /**
+     * 购物车中添加商品
+     */
 	@RequestMapping("/addProd")
 	public ResponseData<CartProdOptRes> addProd(HttpSession session, @RequestParam Long buyNum, @RequestParam String skuId) {
 		//获取service
@@ -50,5 +56,21 @@ public class ShopCartController {
 			LOG.error("添加购物车商品出错",e);
         }
 		return responseData;
+	}
+	/**
+	 * 购物车信息概览
+	 */
+	@RequestMapping("/cartDetails")
+	public ModelAndView queryCartDetails(HttpSession session){
+		//获取service
+		IShopCartSV iShopCartSV = DubboConsumerFactory.getService("IShopCartSV");
+		UserInfo userInfo = new UserInfo();
+//		userInfo.setTenantId((String)session.getAttribute("tenantId"));
+//		userInfo.setUserId((String)session.getAttribute("userId"));
+		List<CartProdInfo> cartProdInfoList = iShopCartSV.queryCartOfUser(userInfo);
+		Map<String,String> model = new HashMap<>();
+		String cartProdJson = JSonUtil.toJSon(cartProdInfoList);
+		model.put("cartProdList", cartProdJson);
+		return new ModelAndView("jsp/shopcart/shopping_cart",model);
 	}
 }
