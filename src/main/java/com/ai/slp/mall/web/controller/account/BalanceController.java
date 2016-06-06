571ee417412892e2a3c364232136c002968fc008
zhangzhongde <xtgjzzd@163.com>
2016-06-06 15:57:38 +0800

    修改gradle

--- src/main/java/com/ai/slp/mall/web/controller/account/BalanceController.java
+++ src/main/java/com/ai/slp/mall/web/controller/account/BalanceController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,17 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
-import com.ai.opt.sdk.web.model.ResponseData;
+import com.ai.opt.sdk.util.DateUtil;
 import com.ai.slp.balance.api.fundquery.interfaces.IFundQuerySV;
 import com.ai.slp.balance.api.fundquery.param.AccountIdParam;
 import com.ai.slp.balance.api.fundquery.param.FundInfo;
-import com.ai.slp.web.frame.velocity.VelocityBuilder;
+import com.ai.slp.charge.api.paymentquery.interfaces.IPaymentQuerySV;
+import com.ai.slp.charge.api.paymentquery.param.ChargeInfoQueryByAcctIdParam;
 import com.alibaba.fastjson.JSON;
 
 @RestController
 public class BalanceController {
-	@Autowired
-	private VelocityBuilder velocityBuilder;
 	//
 	private static final String ACCOUNT_ID = "";
 	private static final String TENANT_ID = "";
@@ -34,10 +32,6 @@ public class BalanceController {
 		Map<String,Object> paramMap = new HashMap<String,Object>();
 		paramMap.put("linkModel", "accountBalance");
 		paramMap.put("testName", "zhangzd");
-		String realPath = request.getRealPath("/jsp/account/balance/账户余额.html");
-		String str2 = this.velocityBuilder.doBuildTemplate(paramMap, realPath);
-		System.out.println("str2:-------------->>>>:"+str2);
-		System.out.println("str2Json:-------------->>>>:"+JSON.toJSONString(str2));
         ModelAndView view = new ModelAndView("jsp/account/balance/index",paramMap);
         return view;
     }
@@ -87,11 +81,10 @@ public class BalanceController {
 	@RequestMapping(value="/account/test",method = RequestMethod.POST, produces = "text/html;charset=UTF-8")
 	@ResponseBody
 	public String test(HttpServletRequest request) {
+		String str2 = "str2";
 		Map<String,Object> paramMap = new HashMap<String,Object>();
 		paramMap.put("linkModel", "accountBalance");
 		paramMap.put("testName", "zhangzd");
-		String realPath = request.getRealPath("/jsp/account/balance/账户余额.html");
-		String str2 = this.velocityBuilder.doBuildTemplate(paramMap, realPath);
 		paramMap.put("str2", str2);
 		System.out.println("paramMapJson:-------------->>>>:"+JSON.toJSONString(paramMap));
 		System.out.println("str2:-------------->>>>:"+str2);
@@ -117,5 +110,25 @@ public class BalanceController {
 		//
 		return balance.toString();
     }
-	
+	/**
+	 * 查询近七天收支记录
+	 * @param request
+	 * @return
+	 * @author zhangzd
+	 * @ApiDocMethod
+	 * @ApiCode
+	 */
+	@RequestMapping(value="/account/queryChargeBaseInfoByAcctId",method = RequestMethod.POST, produces = "text/html;charset=UTF-8")
+	@ResponseBody
+	public String queryChargeBaseInfoByAcctId(HttpServletRequest request) {
+		ChargeInfoQueryByAcctIdParam chargeInfoQueryByAcctIdParam = new ChargeInfoQueryByAcctIdParam();
+		chargeInfoQueryByAcctIdParam.setAccountId(new Long(ACCOUNT_ID));
+		chargeInfoQueryByAcctIdParam.setTenantId(TENANT_ID);
+		chargeInfoQueryByAcctIdParam.setStartTime(DateUtil.getSysDate());
+		chargeInfoQueryByAcctIdParam.setEndTime(DateUtil.getSysDate());
+		//
+		DubboConsumerFactory.getService(IPaymentQuerySV.class).queryChargeBaseInfoByAcctId(chargeInfoQueryByAcctIdParam);
+		//
+		return null;
+    }
 }
