beeb5fe48667c9c006c0439dbcf362aa4d45a4f1
gaogang <gaogang@asiainfo.com>
2016-06-08 09:56:18 +0800

    update

--- src/main/java/com/ai/slp/mall/web/controller/order/OrderController.java
+++ src/main/java/com/ai/slp/mall/web/controller/order/OrderController.java
@@ -69,52 +69,53 @@ public class OrderController {
 		String payStyleParams = JSonUtil.toJSon(payStyleParamList);
 		List<SysParam> orderStyleParamList = iCacheSV.getSysParams("SLP", "ORD_ORDER", "ORDER_TYPE");
 		String orderStyleParams = JSonUtil.toJSon(orderStyleParamList);
-		Map<String, String> model = new HashMap<String,String>();
-		model.put("payStyleParams",payStyleParams);
-		model.put("orderStyleParams",orderStyleParams);
-		return new ModelAndView("jsp/order/order_list",model);
+		Map<String, String> model = new HashMap<String, String>();
+		model.put("payStyleParams", payStyleParams);
+		model.put("orderStyleParams", orderStyleParams);
+		return new ModelAndView("jsp/order/order_list", model);
 	}
-	
+
 	@RequestMapping("/getOrderListData")
 	@ResponseBody
-	public ResponseData<PageInfo<OrdOrderVo>> getOrderListData(HttpServletRequest request,OrderListQueryParams queryParams ){
+	public ResponseData<PageInfo<OrdOrderVo>> getOrderListData(HttpServletRequest request,
+			OrderListQueryParams queryParams) {
 		ResponseData<PageInfo<OrdOrderVo>> responseData = null;
 		try {
 			String searchType = queryParams.getSearchType();
 			QueryOrderListRequest queryRequest = new QueryOrderListRequest();
 			BeanUtils.copyProperties(queryRequest, queryParams);
-			if("1".equals(searchType)){
+			if ("1".equals(searchType)) {
 				String selectTime = queryParams.getSelectTime();
 				queryRequest.setOrderTimeBegin(null);
 				queryRequest.setOrderTimeEnd(null);
-				if("1".equals(selectTime)){//3月内
+				if ("1".equals(selectTime)) {// 3月内
 					String startDateStr = getBeforeMonthDate(3);
 					queryRequest.setOrderTimeBegin(startDateStr);
 					String endDateStr = DateUtil.getDateString("yyyy-MM-dd HH:mm:ss");
 					queryRequest.setOrderTimeEnd(endDateStr);
-				}else if("2".equals(selectTime)){//当年
+				} else if ("2".equals(selectTime)) {// 当年
 					String startDateStr = getYearBeginDate();
 					queryRequest.setOrderTimeBegin(startDateStr);
 					String endDateStr = DateUtil.getDateString("yyyy-MM-dd HH:mm:ss");
 					queryRequest.setOrderTimeEnd(endDateStr);
 				}
-			}else{
+			} else {
 				String orderTimeBegin = queryRequest.getOrderTimeBegin();
-				if(!StringUtil.isBlank(orderTimeBegin)){
-					queryRequest.setOrderTimeBegin(orderTimeBegin+" 00:00:00");
+				if (!StringUtil.isBlank(orderTimeBegin)) {
+					queryRequest.setOrderTimeBegin(orderTimeBegin + " 00:00:00");
 				}
 				String orderTimeEnd = queryRequest.getOrderTimeEnd();
-				if(!StringUtil.isBlank(orderTimeEnd)){
-					queryRequest.setOrderTimeEnd(orderTimeEnd+" 23:59:59");
+				if (!StringUtil.isBlank(orderTimeEnd)) {
+					queryRequest.setOrderTimeEnd(orderTimeEnd + " 23:59:59");
 				}
 			}
 			IOrderListSV iOrderListSV = DubboConsumerFactory.getService("iOrderListSV");
 			QueryOrderListResponse orderListResponse = iOrderListSV.queryOrderList(queryRequest);
-			if(orderListResponse != null && orderListResponse.getResponseHeader().isSuccess()){
-				PageInfo<OrdOrderVo> pageInfo=orderListResponse.getPageInfo();
+			if (orderListResponse != null && orderListResponse.getResponseHeader().isSuccess()) {
+				PageInfo<OrdOrderVo> pageInfo = orderListResponse.getPageInfo();
 				setProductImageUrl(pageInfo);
 				responseData = new ResponseData<PageInfo<OrdOrderVo>>(ExceptionCode.SUCCESS, "查询成功", pageInfo);
-			}else{
+			} else {
 				responseData = new ResponseData<PageInfo<OrdOrderVo>>(ExceptionCode.SYSTEM_ERROR, "查询失败", null);
 			}
 		} catch (Exception e) {
@@ -127,17 +128,18 @@ public class OrderController {
 
 	/**
 	 * 设置商品图片
+	 * 
 	 * @param pageInfo
 	 */
 	private void setProductImageUrl(PageInfo<OrdOrderVo> pageInfo) {
 		// 获取imageClient
 		IImageClient imageClient = IDPSClientFactory.getImageClient(ProductImageConstant.IDPSNS);
 		List<OrdOrderVo> orderList = pageInfo.getResult();
-		if(orderList != null && orderList.size()>0){
-			for(OrdOrderVo orderVo : orderList){
+		if (orderList != null && orderList.size() > 0) {
+			for (OrdOrderVo orderVo : orderList) {
 				List<OrdProductVo> productList = orderVo.getProductList();
-				if(productList != null && productList.size()>0){
-					for(OrdProductVo productVo: productList){
+				if (productList != null && productList.size() > 0) {
+					for (OrdProductVo productVo : productList) {
 						ProductImage productImage = productVo.getProductImage();
 						String picType = productImage.getPicType();
 						String vfsId = productImage.getVfsId();
@@ -148,9 +150,10 @@ public class OrderController {
 			}
 		}
 	}
-	
+
 	/**
 	 * 获得本年开始日期
+	 * 
 	 * @param beforeMonth
 	 * @return
 	 */
@@ -158,11 +161,12 @@ public class OrderController {
 		String dateString = DateUtil.getDateString("yyyy-MM-dd");
 		String[] dataArray = dateString.split("-");
 		int year = Integer.parseInt(dataArray[0]);
-		return year+"-01-01 00:00:00";
+		return year + "-01-01 00:00:00";
 	}
 
 	/**
 	 * 获得提前几月日期（1号）
+	 * 
 	 * @param beforeMonth
 	 * @return
 	 */
@@ -173,48 +177,49 @@ public class OrderController {
 		int month = Integer.parseInt(dataArray[1]);
 		String startDateStr = null;
 		int startMonth = month;
-		if(month>2){
-			startMonth = month - (beforeMonth-1);
-		}else{
-			startMonth = 12 + (month-(beforeMonth-1));
+		if (month > 2) {
+			startMonth = month - (beforeMonth - 1);
+		} else {
+			startMonth = 12 + (month - (beforeMonth - 1));
 		}
-		if(startMonth<10){
-			startDateStr = year+"-0"+ startMonth +"-01 00:00:00";
-		}else{
-			startDateStr = year+"-"+ startMonth +"-01 00:00:00";
+		if (startMonth < 10) {
+			startDateStr = year + "-0" + startMonth + "-01 00:00:00";
+		} else {
+			startDateStr = year + "-" + startMonth + "-01 00:00:00";
 		}
 		return startDateStr;
 	}
-	
+
 	@RequestMapping("/detail")
-	public ModelAndView orderDetail(HttpServletRequest request,QueryOrderRequest orderRequest) {
+	public ModelAndView orderDetail(HttpServletRequest request, QueryOrderRequest orderRequest) {
 		OrdOrderVo orderDetail = getOrderDetail(request, orderRequest);
-		Map<String,String> model  = new HashMap<String,String>();
-		if(orderDetail != null){
+		Map<String, String> model = new HashMap<String, String>();
+		if (orderDetail != null) {
 			String orderJSon = JSonUtil.toJSon(orderDetail);
 			model.put("orderDetail", orderJSon);
 		}
 		String orderType = request.getParameter("orderType");
-		if(StringUtils.isContains("100010,100011", orderType)){
-			return new ModelAndView("jsp/order/order_info_detail",model);
-		}else{
-			return new ModelAndView("jsp/order/order_product_detail",model);
+		if (StringUtils.isContains("100010,100011", orderType)) {
+			return new ModelAndView("jsp/order/order_info_detail", model);
+		} else {
+			return new ModelAndView("jsp/order/order_product_detail", model);
 		}
 	}
-	
+
 	/**
 	 * 订单详情查询
+	 * 
 	 * @param request
 	 * @param orderRequest
 	 * @return
 	 */
-	private OrdOrderVo getOrderDetail(HttpServletRequest request,QueryOrderRequest orderRequest){
+	private OrdOrderVo getOrderDetail(HttpServletRequest request, QueryOrderRequest orderRequest) {
 		OrdOrderVo responseData = null;
 		try {
 			orderRequest.setTenantId("SLP");
 			IOrderListSV iOrderListSV = DubboConsumerFactory.getService("iOrderListSV");
 			QueryOrderResponse orderInfo = iOrderListSV.queryOrder(orderRequest);
-			if(orderInfo != null && orderInfo.getResponseHeader().isSuccess()){
+			if (orderInfo != null && orderInfo.getResponseHeader().isSuccess()) {
 				responseData = orderInfo.getOrdOrderVo();
 			}
 		} catch (Exception e) {
@@ -222,6 +227,7 @@ public class OrderController {
 		}
 		return responseData;
 	}
+
 	/**
 	 * 下单并且跳转到支付页面
 	 */
@@ -236,40 +242,47 @@ public class OrderController {
 		ResponseData<String> resData = null;
 		try {
 			HttpSession session = request.getSession();
-			
-	        SLPClientUser user = (SLPClientUser) session.getAttribute(SSOClientConstants.USER_SESSION_KEY);
-	        if(null==user){
-	        	orderReq.setUserId("900000000000000000");//先写死
-	        }else{
-	        	orderReq.setUserId(user.getUserId());
-	        }
+
+			SLPClientUser user = (SLPClientUser) session.getAttribute(SSOClientConstants.USER_SESSION_KEY);
+			if (null == user) {
+				orderReq.setUserId("900000000000000000");// 先写死
+			} else {
+				orderReq.setUserId(user.getUserId());
+			}
 
 			String orderKey = UUIDUtil.genId32();
-			
+
 			CacheUtil.setValue(orderKey, 300, orderReq, SLPMallConstants.Order.CACHE_NAMESPACE);
-			resData=new ResponseData<String>(ExceptionCode.SUCCESS, "查询成功", orderKey);
-			
+			resData = new ResponseData<String>(ExceptionCode.SUCCESS, "查询成功", orderKey);
+
 		} catch (Exception e) {
-			resData=new ResponseData<String>(ExceptionCode.SYSTEM_ERROR, "查询失败", null);
+			resData = new ResponseData<String>(ExceptionCode.SYSTEM_ERROR, "查询失败", null);
 		}
-		
-		
+
 		return resData;
 
 	}
+
 	@RequestMapping("/toOrderPay")
-	public String toOrderPay(HttpServletRequest request,Model model){
-		String orderKey=request.getParameter("orderKey");
-		PayOrderRequest res=(PayOrderRequest)CacheUtil.getValue(orderKey, SLPMallConstants.Order.CACHE_NAMESPACE, PayOrderRequest.class);
-		OrderTradeCenterRequest orderrequest=new OrderTradeCenterRequest();
-		orderrequest.setTenantId("SLP");
-		OrdBaseInfo baseInfo=new OrdBaseInfo();
+	public String toOrderPay(HttpServletRequest request, Model model) {
+		String orderKey = request.getParameter("orderKey");
+		PayOrderRequest res = (PayOrderRequest) CacheUtil.getValue(orderKey, SLPMallConstants.Order.CACHE_NAMESPACE,
+				PayOrderRequest.class);
+		OrderTradeCenterRequest orderrequest = new OrderTradeCenterRequest();
+		SLPClientUser user = (SLPClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
+		if (null == user) {
+			orderrequest.setTenantId("SLP");
+		} else {
+			orderrequest.setTenantId(user.getTenantId());
+		}
+
+		OrdBaseInfo baseInfo = new OrdBaseInfo();
 		baseInfo.setUserId(res.getUserId());
 		baseInfo.setOrderType(res.getOrderType());
 		orderrequest.setOrdBaseInfo(baseInfo);
-		
-		List<OrdProductInfo> list=new ArrayList<OrdProductInfo>();
-		OrdProductInfo opInfo=new OrdProductInfo();
+
+		List<OrdProductInfo> list = new ArrayList<OrdProductInfo>();
+		OrdProductInfo opInfo = new OrdProductInfo();
 		opInfo.setBasicOrgId(res.getBasicOrgId());
 		opInfo.setBuySum(Integer.valueOf(res.getBuySum()));
 		opInfo.setProvinceCode(res.getProvinceCode());
@@ -277,29 +290,29 @@ public class OrderController {
 		opInfo.setChargeFee(res.getChargeFee());
 		list.add(opInfo);
 		orderrequest.setOrdProductInfoList(list);
-		OrdExtendInfo exInfo=new OrdExtendInfo();
-        List<ProdExtendInfoVo> listVo=new ArrayList<ProdExtendInfoVo>();
-		InfoJsonVo vo=new InfoJsonVo();
-		ProdExtendInfoVo pvo=new ProdExtendInfoVo();
+		OrdExtendInfo exInfo = new OrdExtendInfo();
+		List<ProdExtendInfoVo> listVo = new ArrayList<ProdExtendInfoVo>();
+		InfoJsonVo vo = new InfoJsonVo();
+		ProdExtendInfoVo pvo = new ProdExtendInfoVo();
 		pvo.setProdExtendInfoValue(res.getPhoneNum());
 		listVo.add(pvo);
 		vo.setProdExtendInfoVoList(listVo);
 		exInfo.setInfoJson(JSON.toJSONString(vo));
 		orderrequest.setOrdExtendInfo(exInfo);
-		IOrderTradeCenterSV iOrderTradeCenterSV=DubboConsumerFactory.getService(com.ai.slp.order.api.ordertradecenter.interfaces.IOrderTradeCenterSV.class);
-		 OrderTradeCenterResponse response=iOrderTradeCenterSV.apply(orderrequest);
-		 List<OrdProductResInfo> ordProductResList = response.getOrdProductResList();
-		 OrdFeeInfo ordFeeInfo = response.getOrdFeeInfo();
-		 model.addAttribute("ordProductResList",ordProductResList);
-		 model.addAttribute("ordFeeInfo", ordFeeInfo);
-		 model.addAttribute("orderId", response.getOrderId());
-		 model.addAttribute("expFee", 0);
-		 model.addAttribute("balanceFee", 0);
-		 model.addAttribute("balance", 0);
-		 
-		System.out.println("____>"+JSON.toJSONString(response));
+		IOrderTradeCenterSV iOrderTradeCenterSV = DubboConsumerFactory
+				.getService(com.ai.slp.order.api.ordertradecenter.interfaces.IOrderTradeCenterSV.class);
+		OrderTradeCenterResponse response = iOrderTradeCenterSV.apply(orderrequest);
+		List<OrdProductResInfo> ordProductResList = response.getOrdProductResList();
+		OrdFeeInfo ordFeeInfo = response.getOrdFeeInfo();
+		model.addAttribute("ordProductResList", ordProductResList);
+		model.addAttribute("ordFeeInfo", ordFeeInfo);
+		model.addAttribute("orderId", response.getOrderId());
+		model.addAttribute("expFee", 0);
+		model.addAttribute("balanceFee", 0);
+		model.addAttribute("balance", 0);
+
+		System.out.println("____>" + JSON.toJSONString(response));
 		return "jsp/order/order_submit";
 	}
-	
-	
+
 }
