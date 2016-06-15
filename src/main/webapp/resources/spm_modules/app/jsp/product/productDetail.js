ca83025ab00f24a9bfc85996b0666749a138abc9
jiaxiaoshuo <jiaxs@asiainfo.com>
2016-06-15 15:32:56 +0800

    商品详情热门推荐修改

--- src/main/webapp/resources/spm_modules/app/jsp/product/productDetail.js
+++ src/main/webapp/resources/spm_modules/app/jsp/product/productDetail.js
@@ -223,13 +223,13 @@ define('app/jsp/product/productDetail', function (require, exports, module) {
         },
         _getHotProduct:function(){
         	$("#hotProductData").html("");
-      		ajaxController.ajax({
+      		$.ajax({
 						type: "post",
 						dataType: "json",
 						processing: false,
 						//message: "查询中，请等待...",
 						url: _base+"/search/getHotProduct",
-						data:{areaCode:$("#currentCity").attr("currentCityCode"),productCatId:productCatId},
+						data:{productCatId:productCatId},
 						success: function(data){
 							if(data.data){
 								var template = $.templates("#hotProductListTmpl");
