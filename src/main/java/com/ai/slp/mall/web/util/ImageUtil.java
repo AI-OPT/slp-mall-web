4bd7eaef979d898b3f3e7db872662e9a1d827d18
Zh <Zh@10.1.64.66>
2016-06-16 10:52:16 +0800

    调整js

--- src/main/java/com/ai/slp/mall/web/util/ImageUtil.java
+++ src/main/java/com/ai/slp/mall/web/util/ImageUtil.java
@@ -8,42 +8,60 @@ import com.ai.opt.sdk.components.idps.IDPSClientFactory;
 import com.ai.paas.ipaas.image.IImageClient;
 
 public class ImageUtil {
-   public static String getImage(String vsid,String pictype){
-       IImageClient im = null;
-       //应用场景
-       String idpsns="slp-mall-web-idps";
-       //获取imageClient
-       im = IDPSClientFactory.getImageClient(idpsns);
-      //获取上传图片的URL
-       return im.getImageUrl(vsid, "."+pictype);
-   }
-   public static String getHotImage(){
-       IImageClient im = null;
-       //应用场景
-       String idpsns="slp-mall-web-idps";
-       //获取imageClient
-       im = IDPSClientFactory.getImageClient(idpsns);
-      //获取上传图片的URL
-       return im.getImageUrl("574558c6d601800009c0b0e5", ".jpg");
-       //获取上传图片指定尺寸的URL
-      // System.out.println(im.getImageUrl("574514c1d601800009c0b0ba", ".jpg","100x80"));
-   }
-   public static List<String> getImages(Map<String,String> imgMap){
-       IImageClient im = null;
-       //应用场景
-       String idpsns="slp-mall-web-idps";
-       //获取imageClient
-       im = IDPSClientFactory.getImageClient(idpsns);
-       List<String> list = new ArrayList<String>();
-       //获取上传图片指定尺寸的URL
-       for (Map.Entry<String, String> entry : imgMap.entrySet()) {
-           String url = im.getImageUrl(entry.getKey(), "."+entry.getValue(),"100x80"); 
-           list.add(url);
-         }
-      /* for(String visIDs:imgList){
-           String url = im.getImageUrl(visIDs, ".jpg","100x80"); 
-           list.add(url);
-       }*/
-       return list;
-   }
+    public static String getImage(String vsid, String pictype) {
+        IImageClient im = null;
+        // 应用场景
+        String idpsns = "slp-mall-web-idps";
+        // 获取imageClient
+        im = IDPSClientFactory.getImageClient(idpsns);
+        // 获取上传图片的URL
+        return im.getImageUrl(vsid, "." + pictype);
+    }
+    
+    public static String getHotImage() {
+        IImageClient im = null;
+        // 应用场景
+        String idpsns = "slp-mall-web-idps";
+        // 获取imageClient
+        im = IDPSClientFactory.getImageClient(idpsns);
+        // 获取上传图片的URL
+        return im.getImageUrl("574558c6d601800009c0b0e5", ".jpg");
+        // 获取上传图片指定尺寸的URL
+        // System.out.println(im.getImageUrl("574514c1d601800009c0b0ba", ".jpg","100x80"));
+    }
+
+    public static List<String> getImages(Map<String, String> imgMap) {
+        IImageClient im = null;
+        // 应用场景
+        String idpsns = "slp-mall-web-idps";
+        // 获取imageClient
+        im = IDPSClientFactory.getImageClient(idpsns);
+        List<String> list = new ArrayList<String>();
+        // 获取上传图片指定尺寸的URL
+        for (Map.Entry<String, String> entry : imgMap.entrySet()) {
+            String url = im.getImageUrl(entry.getKey(), "." + entry.getValue(), "100x80");
+            list.add(url);
+        }
+        /*
+         * for(String visIDs:imgList){ String url = im.getImageUrl(visIDs, ".jpg","100x80");
+         * list.add(url); }
+         */
+        return list;
+    }
+
+    //上传图片
+    public static String AddImg(byte[] imgByte,String imgName) {
+        // 应用场景
+        String idpsns = "slp-mall-web-idps";
+        // 获取imageClient
+        IImageClient im = IDPSClientFactory.getImageClient(idpsns);
+
+        // 上传图片，获取上传后的ID
+        String idpsId = im.upLoadImage(imgByte, imgName);
+        // 获取上传图片的URL
+        System.out.println(im.getImageUrl(idpsId, ".jpg"));
+        // 获取上传图片指定尺寸的URL
+        return im.getImageUrl(idpsId, ".jpg", "100x80");
+
+    }
 }
